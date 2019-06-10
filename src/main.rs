f114552e23ee75b2e0e7e3320f8db34d4e6aca3a
ruthaleks <ruth.aleks@gmail.com>
2019-06-10 15:14:18 +0200

    allowing multiple guesses and exit when otaining correct guess

--- src/main.rs
+++ src/main.rs
@@ -14,42 +14,46 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1,101);
     
     println!("The secret number in: {}", secret_number); 
+    loop {
+        println!("Please input you guess");
 
-    println!("Please input you guess");
-
-    /* let - used to create a variable, in Rust variables are immutable (unchangeble) by default
-     * mut - makes the variable mutable 
-     * String::new - function that retuns a new instance of a String 
-     * String - string type provided ny std library
-     * :: - indicates an associated function of the String type (static method, performed on a type
-     * and not a particular instance)
-     * new - funtion creates a new empty string */
-    let mut guess = String::new(); // mutable variable that is currently bound to a new, empty instance of a String
+        /* let - used to create a variable, in Rust variables are immutable (unchangeble) by default
+        * mut - makes the variable mutable 
+        * String::new - function that retuns a new instance of a String 
+        * String - string type provided ny std library
+        * :: - indicates an associated function of the String type (static method, performed on a type
+        * and not a particular instance)
+        * new - funtion creates a new empty string */
+        let mut guess = String::new(); // mutable variable that is currently bound to a new, empty instance of a String
     
-    /* stdin - function from std::io module
-     * .read_line(&mut guess) - calls the real_line method on the input handle to get input from
-     * the user with the argument &mut guess
-     * & - indicates that this argument is a reference and is by default immutable, reference is
-     * made mutable with &mut guess rather than &guess 
-     * .expect() - error handling */
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line..");
+        /* stdin - function from std::io module
+        * .read_line(&mut guess) - calls the real_line method on the input handle to get input from
+        * the user with the argument &mut guess
+        * & - indicates that this argument is a reference and is by default immutable, reference is
+        * made mutable with &mut guess rather than &guess 
+        * .expect() - error handling */
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line..");
 
-    /* create a new variable named guess that shadows that previous value of guess with a new type
-     * trim - method on the String guess that eliminate any whitespace at the beginning and end and
-     * also \n (newline)
-     * parse - parses a string into some kind of number type, in this cas u32*/
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        /* create a new variable named guess that shadows that previous value of guess with a new type
+        * trim - method on the String guess that eliminate any whitespace at the beginning and end and
+        * also \n (newline)
+        * parse - parses a string into some kind of number type, in this cas u32*/
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
     
-    /* {} - placeholder */
-    println!("You guessed: {}", guess);
+        /* {} - placeholder */
+        println!("You guessed: {}", guess);
 
-    /* match - is an expresion made up of arms which is the code that will be run if value match of
-     * the arm expresion*/
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win"),
+        /* match - is an expresion made up of arms which is the code that will be run if value match of
+        * the arm expresion*/
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
     }
 }
